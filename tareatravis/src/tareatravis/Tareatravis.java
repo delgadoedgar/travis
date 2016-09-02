ac7da2c1727d339ff14d61df498e5db5627633e8
USUARIO-PC\USUARIO <edgarmauriciodd@gmail.com>
2016-09-01 21:25:38 -0500

    s4

--- tareatravis/src/tareatravis/Tareatravis.java
+++ tareatravis/src/tareatravis/Tareatravis.java
@@ -42,7 +42,13 @@ public class Tareatravis {
                     r=n1-n2;
                     JOptionPane.showMessageDialog(null,"La resta es:"+r);
                     break;
-               
+                case 3:
+                    n1=Double.parseDouble(JOptionPane.showInputDialog("Ingrese numero 1"));
+                    n2=Double.parseDouble(JOptionPane.showInputDialog("Ingrese numero 2"));
+                    m=n1*n2;
+                    JOptionPane.showMessageDialog(null,"La multiplicacion es:"+m);
+                    break;
+              
 
             }
         }while(op!=5);
