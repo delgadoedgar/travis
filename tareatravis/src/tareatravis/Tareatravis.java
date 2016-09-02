170a5c6af2aa570e4c2bcc634d1b212a5e415ce9
USUARIO-PC\USUARIO <edgarmauriciodd@gmail.com>
2016-09-01 21:17:28 -0500

    prime

--- tareatravis/src/tareatravis/Tareatravis.java
+++ tareatravis/src/tareatravis/Tareatravis.java
@@ -16,23 +16,23 @@ public class Tareatravis {
      * @param args the command line arguments
      */
     public static void main(String[] args) 
-    {
-    
-        // TODO code application logic here
-        
-        int op= 0;
-        double n1, n2, s,m,d,r;
+    { int op=0;
+        double n1,n2,s,m,d,r;
         do{
-        op=Integer.parseInt(JOptionPane.showInputDialog("nCalculadoran"+
+            op=Integer.parseInt(JOptionPane.showInputDialog("nCalculadoran"+
             "************n"+
-                "[1] SUMARn"+
+            "[1] SUMARn"+
             "[2] RESTARn"+
             "[3] MULTIPLICARn"+
             "[4] DIVIDIRn"+
             "[5] SALIRn"+
             "Ingresa una opcion:"));
-        switch(op)
-        {
-        
+
+            switch(op)
+            {
+                
+            }
+        }while(op!=5);
     }
-  }
+
+}
