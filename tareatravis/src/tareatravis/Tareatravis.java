03994bd82a5743307afd37b00b188b224de6a6fe
USUARIO-PC\USUARIO <edgarmauriciodd@gmail.com>
2016-09-01 21:13:03 -0500

    n

---
+++ tareatravis/src/tareatravis/Tareatravis.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareatravis;
+import javax.swing.*;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tareatravis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+    
+        // TODO code application logic here
+        
+        int op= 0;
+        double n1, n2, s,m,d,r;
+        do{
+        op=Integer.parseInt(JOptionPane.showInputDialog("nCalculadoran"+
+            "************n"+
+                "[1] SUMARn"+
+            "[2] RESTARn"+
+            "[3] MULTIPLICARn"+
+            "[4] DIVIDIRn"+
+            "[5] SALIRn"+
+            "Ingresa una opcion:"));
+        switch(op)
+        {
+        
+    }
+  }
